202e0a6d3d8b9cbb7480e73b14c7bf46fb4cc9b2
Anas8e <94750646+Anas8e@users.noreply.github.com>
2023-09-23 00:34:33 +0100

    1update

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,7 @@
-# Install puppet-lint
-package { 'puppet-lint':
-  ensure   => '2.5.0',
-  provider => 'gem'
+# 1-install_a_package.pp
+
+# Ensure the package is installed and at the required version
+package { 'Flask':
+  ensure => '2.1.0',
+  provider => 'pip3',
 }
