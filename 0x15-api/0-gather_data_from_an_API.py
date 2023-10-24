7d2dd2a87964552a97540a57e6098c779761ecc0
Anas8e <94750646+Anas8e@users.noreply.github.com>
2023-10-24 01:08:26 +0100

    Up

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,18 +1,29 @@
 #!/usr/bin/python3
-"""script using this REST API, for a given employee ID,
-returns information about his/her TODO list progress."""
-import requests as r
+"""Script to gather employee TODO list progress from an API"""
+
+import requests
 import sys
 
-if __name__ == '__main__':
-    url = 'https://jsonplaceholder.typicode.com/'
-    usr_id = r.get(url + 'users/{}'.format(sys.argv[1])).json()
-    to_do = r.get(url + 'todos', params={'userId': sys.argv[1]}).json()
-#    print(to_do)
-    completed = [title.get("title") for title in to_do if
-                 title.get('completed') is True]
-    print(completed)
-    print("Employee {} is done with tasks({}/{}):".format(usr_id.get("name"),
-                                                          len(completed),
-                                                          len(to_do)))
-    [print("\t {}".format(title)) for title in completed]
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: {} <employee_id>".format(sys.argv[0]))
+        sys.exit(1)
+
+    employee_id = int(sys.argv[1])
+    base_url = "https://jsonplaceholder.typicode.com"
+
+    # Fetch user information
+    user_response = requests.get("{}/users/{}".format(base_url, employee_id))
+    user_data = user_response.json()
+    employee_name = user_data.get("name")
+
+    # Fetch user's TODO list
+    todo_response = requests.get("{}/todos?userId={}".format(base_url, employee_id))
+    todo_data = todo_response.json()
+
+    completed_tasks = [task for task in todo_data if task.get("completed")]
+    total_tasks = len(todo_data)
+
+    print("Employee {} is done with tasks({}/{}):".format(employee_name, len(completed_tasks), total_tasks))
+    for task in completed_tasks:
+        print("\t {}".format(task.get("title")))
