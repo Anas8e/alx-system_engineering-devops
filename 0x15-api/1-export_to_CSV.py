aadddf6fdce5c8096cbb2e7d9e932c4f379a2523
Anas8e <94750646+Anas8e@users.noreply.github.com>
2023-10-24 01:33:17 +0100

    ttt

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,38 +1,57 @@
 #!/usr/bin/python3
 """
-Return information for a given employee about his/her TODO list progress
+Export employee's TODO list data to CSV format.
 """
+
+import requests
+import sys
+import csv
+
+def export_todo_list_to_csv(employee_id):
+    # Define the base URL for the API
+    base_url = "https://jsonplaceholder.typicode.com"
+    
+    # Fetch user information
+    user_response = requests.get(f"{base_url}/users/{employee_id}")
+    user_data = user_response.json()
+    
+    if "id" not in user_data:
+        print(f"User with ID {employee_id} not found")
+        return
+    
+    user_id = user_data["id"]
+    username = user_data["username"]
+    
+    # Fetch user's TODO list
+    todo_response = requests.get(f"{base_url}/todos", params={"userId": user_id})
+    todo_data = todo_response.json()
+    
+    if not todo_data:
+        print(f"No TODO list data found for user {username}")
+        return
+    
+    # Define the CSV file name
+    filename = f"{user_id}.csv"
+    
+    # Export TODO list data to CSV
+    with open(filename, "w", newline="") as csvfile:
+        csv_writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        
+        # Write the header row
+        csv_writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
+        
+        # Write each TODO item to the CSV
+        for todo in todo_data:
+            task_completed_status = "True" if todo["completed"] else "False"
+            task_title = todo["title"]
+            csv_writer.writerow([user_id, username, task_completed_status, task_title])
+    
+    print(f"Data exported to {filename}")
+
 if __name__ == "__main__":
-    import csv
-    import requests
-    import sys
-
-    DONE_TASKS = 0
-    ALL_TASKS = 0
-    csvList = []
-
-    URL_FOR_USERS = 'https://jsonplaceholder.typicode.com/users/{0}'.\
-        format(sys.argv[1])
-    URL_FOR_TODOS = 'https://jsonplaceholder.typicode.com/todos'
-    r_for_users = requests.get(URL_FOR_USERS)
-    r_for_todos = requests.get(URL_FOR_TODOS)
-
-    name = r_for_users.json().get('name')
-    user_name = r_for_users.json().get('username')
-    todos = r_for_todos.json()
-    for todo in todos:
-        if todo.get('userId') == int(sys.argv[1]):
-            subcsvList = []
-        if todo.get('userId') == int(sys.argv[1]):
-            ALL_TASKS += 1
-            subcsvList.append(todo.get('userId'))
-            subcsvList.append(user_name)
-            subcsvList.append(todo.get('completed'))
-            subcsvList.append(todo.get('title'))
-            csvList.append(subcsvList)
-        if (todo.get('userId') == int(sys.argv[1]))\
-                and (todo.get('completed')):
-            DONE_TASKS += 1
-    with open(sys.argv[1] + '.csv', 'w+', newline='') as f:
-        writer = csv.writer(f, quoting=csv.QUOTE_ALL)
-        writer.writerows(csvList)
+    if len(sys.argv) != 2:
+        print("Usage: {} <employee_id>".format(sys.argv[0]))
+        sys.exit(1)
+    
+    employee_id = int(sys.argv[1])
+    export_todo_list_to_csv(employee_id)
