959bc131c058b42f8b986dacd506ec5c5a83898e
Anas8e <94750646+Anas8e@users.noreply.github.com>
2023-11-08 01:02:03 +0100

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,21 +5,25 @@
 import requests
 
 def number_of_subscribers(subreddit):
-    """
-    Function to query the Reddit API and return the number of subscribers for a given subreddit.
-    """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "my-app/0.0.1"}  # Set a custom User-Agent
+    headers = {'User-Agent': 'my_user_agent'}  # Set your User-Agent here
+
     response = requests.get(url, headers=headers)
 
     if response.status_code == 200:
         data = response.json()
-        subscribers = data.get("data", {}).get("subscribers", 0)
+        subscribers = data['data']['subscribers']
         return subscribers
     else:
         return 0
 
 if __name__ == "__main__":
-    subreddit = input("Enter a subreddit: ")
-    num_subscribers = number_of_subscribers(subreddit)
-    print(f"The number of subscribers in '{subreddit}' is: {num_subscribers}")
+    import sys
+
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        subreddit = sys.argv[1]
+        subscribers = number_of_subscribers(subreddit)
+        print(subscribers)
+
