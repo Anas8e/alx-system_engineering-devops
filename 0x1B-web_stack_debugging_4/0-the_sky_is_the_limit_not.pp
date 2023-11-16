32b24fdea7750969c6acbcd48031caf2bca9c13f
Anas8e <94750646+Anas8e@users.noreply.github.com>
2023-11-16 22:31:53 +0100

    Update 0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,19 +1,13 @@
-# Puppet manifest to debug and optimize the Nginx server configuration
+# Increases the amount of traffic an Nginx server can handle.
 
-# Define the Nginx service
-service { 'nginx':
-  ensure => running, # Ensure the service is running
-}
+# Increase the ULIMIT of the default file
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+} ->
 
-# Define the Nginx configuration file
-file { '/etc/nginx/sites-available/default':
-  ensure  => file, # Ensure it's a file
-  content => template('nginx/default.erb'), # Use a template for configuration
-  notify  => Service['nginx'], # Notify the service of changes
-}
-
-# Define a custom Nginx configuration template
-file { '/etc/nginx/default.erb':
-  ensure  => file,
-  content => '# Optimized Nginx configuration goes here',
+# Restart Nginx
+exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
 }
