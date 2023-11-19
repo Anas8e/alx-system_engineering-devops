d11c6a04695117729daf30768d117befb5e07cec
Anas8e <94750646+Anas8e@users.noreply.github.com>
2023-11-19 02:05:13 +0100

    Up

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,10 +1,13 @@
-# Puppet Manifest to Optimize Nginx Configuration
+# Increases the amount of traffic an Nginx server can handle.
 
-exec { 'reload-nginx':
-  command     => '/usr/sbin/service nginx reload',  # Command to reload Nginx service
-  path        => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-  refreshonly => true,                              # Execute only on trigger
-  unless      => '/usr/sbin/service nginx status | grep "not running"', # Check if Nginx is not running
-  logoutput   => true,                              # Log the output of the command
-  notify      => Service['nginx'],                  # Notify the Nginx service
+# Increase the ULIMIT of the default file
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+# Restart Nginx
+exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
 }
