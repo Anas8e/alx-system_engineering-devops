a3ba4a97d69f1257e2ab891aaca7f41dfaa241f9
Anas8e <94750646+Anas8e@users.noreply.github.com>
2024-01-17 12:05:13 +0100

    test PEP8

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,6 +2,7 @@
 """Module for task 0"""
 import requests
 
+
 def number_of_subscribers(subreddit):
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
@@ -10,5 +11,6 @@ def number_of_subscribers(subreddit):
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
         return 0
+
     results = response.json().get("data")
     return results.get("subscribers")
