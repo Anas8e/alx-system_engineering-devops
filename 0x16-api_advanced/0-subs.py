7d495d0f24b4e6460c9f935337760b1d581cfd42
Anas8e <94750646+Anas8e@users.noreply.github.com>
2024-01-17 12:03:22 +0100

    testing last

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,29 +1,14 @@
 #!/usr/bin/python3
-"""
-Module for task 0
-"""
-
+"""Module for task 0"""
 import requests
 
 def number_of_subscribers(subreddit):
-    """
-    Queries the Reddit API and returns the number of subscribers for a given subreddit.
-    
-    Args:
-        subreddit (str): The subreddit name.
-    
-    Returns:
-        int: Number of subscribers, or 0 if invalid subreddit.
-    """
-    # Set custom User-Agent to avoid Too Many Requests errors
-    headers = {'User-Agent': 'Mozilla/5.0'}
-
-    # Make API request
-    response = requests.get(f'https://www.reddit.com/r/{subreddit}/about.json', headers=headers)
-
-    # Check if the subreddit is valid
-    if response.status_code == 200:
-        # Parse JSON response and return subscriber count
-        return response.json()['data']['subscribers']
-    else:
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        'User-Agent': 'linux:0x16.api.adbanced:v1.0.0 (by /u/Anas8e)'
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
         return 0
+    results = response.json().get("data")
+    return results.get("subscribers")
